86155ce360e5c7da72c5bd2696ab76535be296b1
Dev-Gab02 <gaconcepcion@my.cspc.edu.ph>
2026-05-13 23:04:20 +0800

    fixed: update the server file

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,34 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import helmet from 'helmet';
-// import { connectDB } from './config/db.js'
-// import authRoutes from './routes/authRoutes.js'
-// import enrollmentRoutes from './routes/enrollmentRoutes.js'
-// import adminRoutes from './routes/adminRoutes.js'
-
-// dotenv.config();
-
-// const app = express();
-
-// connectDB()
-
-// app.use(cors({
-//   origin: "*"
-// }));
-// app.use(express.json());
-// app.use(helmet());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/enrollment", enrollmentRoutes);
-// app.use("/api/admin", adminRoutes);
-
-// const PORT = process.env.PORT || 5000;
-
-// app.get("/", (req, res) => {
-//   res.send("Backend is running on Azure");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import { connectDB } from './config/db.js'
+import authRoutes from './routes/authRoutes.js'
+import enrollmentRoutes from './routes/enrollmentRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
+
+dotenv.config();
+
 const app = express();
+
+connectDB()
+
+app.use(cors({
+  origin: "*"
+}));
+app.use(express.json());
+app.use(helmet());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/enrollment", enrollmentRoutes);
+app.use("/api/admin", adminRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Backend is alive on Azure");
+  res.send("Enrollment API Running...");
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
